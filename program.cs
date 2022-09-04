5bde6ac933c26c8a26f65ff4eb85705bd8f5144d
Nicolás Gómez <Vlacyadou2017@gmail.com>
2022-09-04 18:24:14 -0300

    cambios

---
+++ program.cs
@@ -0,0 +1,47 @@
+[33mcommit cd686f154eb424e3d60809b7cc1850b59d56b12a[m[33m ([m[1;36mHEAD -> [m[1;32mnicolas[m[33m, [m[1;31morigin/milagrosk[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmain[m[33m)[m
+Author: Nicolás Gómez <Vlacyadou2017@gmail.com>
+Date:   Sun Sep 4 17:55:50 2022 -0300
+
+    ayuda
+
+[33mcommit 649e3dc98b248d77c4b14a91bf7933a845afc991[m
+Author: Nicolás Gómez <Vlacyadou2017@gmail.com>
+Date:   Sun Sep 4 17:55:39 2022 -0300
+
+    program
+
+[33mcommit 3d0dd17815a34a1ca84600a7e9c926260125b814[m
+Author: MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
+Date:   Sun Sep 4 17:51:07 2022 -0300
+
+    program
+
+[33mcommit 2643a9e7550cb736e0ebed033fbadd346cd4801d[m
+Merge: aaeaf1b 85592c4
+Author: MilagrosKucharski <110863912+MilagrosKucharski@users.noreply.github.com>
+Date:   Sun Sep 4 17:40:01 2022 -0300
+
+    Merge pull request #2 from MilagrosKucharski/milagros
+    
+    Merge pull request #1 from MilagrosKucharski/main
+
+[33mcommit 85592c408bfe7b8b3a66c3d0a33358978860cd7e[m[33m ([m[1;31morigin/milagros[m[33m)[m
+Merge: 615d83d aaeaf1b
+Author: MilagrosKucharski <110863912+MilagrosKucharski@users.noreply.github.com>
+Date:   Sun Sep 4 17:39:27 2022 -0300
+
+    Merge pull request #1 from MilagrosKucharski/main
+    
+    hecho
+
+[33mcommit aaeaf1bee6c8cdbda48a93830d022ecc0e26df57[m
+Author: MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
+Date:   Sun Sep 4 17:38:30 2022 -0300
+
+    hecho
+
+[33mcommit 615d83d148dc355d96cb1da6a79a377173fd861a[m[33m ([m[1;31morigin/nicolas[m[33m)[m
+Author: MilagrosKucharski <110863912+MilagrosKucharski@users.noreply.github.com>
+Date:   Sun Sep 4 16:32:59 2022 -0300
+
+    Create README.md
--- src/ej2repos-23/Program.cs
+++ src/ej2repos-23/Program.cs
@@ -1,1 +1,2 @@
 ﻿Console.Writeline("Nicolás Gómez");
+console.Writeline("Milagros Kucharski");
